1b8031aed57f2cc093b05a3566a8c54844755a20
buikhanh2001 <buikhanhhungcong@gmail.com>
2022-11-21 09:37:34 +0700

    firtcommit1st

---
+++ Assets/Scripts/ItemCollector.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ItemCollector : MonoBehaviour
+{
+    private int cherries = 0;
+
+    [SerializeField] private Text cherriesText;
+    [SerializeField] private AudioSource collectionSound;
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Cherry"))
+        {
+            collectionSound.Play();
+            Destroy(collision.gameObject);
+            cherries++;
+            cherriesText.text = "cherries: " + cherries;
+        }
+    }
+}
---
+++ Assets/Scripts/Waypont.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Waypont : MonoBehaviour
+{
+    [SerializeField] private GameObject[] wayponts;
+    private int currentWaypontIndex = 0;
+
+    [SerializeField] private float speed = 2f;
+
+    // Update is called once per frame
+    private void Update()
+    {
+        if (Vector2.Distance(wayponts[currentWaypontIndex].transform.position, transform.position) < .1f)
+        {
+            currentWaypontIndex++;
+            if(currentWaypontIndex >= wayponts.Length)
+            {
+                currentWaypontIndex = 0;
+            }
+        }
+        transform.position = Vector2.MoveTowards(transform.position, wayponts[currentWaypontIndex].transform.position, Time.deltaTime * speed);
+    }
+}
